9d70ac625fd070e1df5c54cb25e0a00863894e56
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-09-04 16:45:59 +0500

    Added event form component

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import EventList from './EventList';
+import EventForm from './EventForm';
 
-export default function App() {
-  return (
-    <EventList />
-  );
-}
+
+const AppNavigator = createStackNavigator({
+  list: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Your Events'
+    })
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: 'Add an Events'
+    })
+  }
+});
+
+const App = createAppContainer(AppNavigator);
+
+export default App
+
+// export default function App() {
+//   return (
+//     <EventList />
+//   );
+// }
 
 
---
+++ EventForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableHighlight } from 'react-native';
+
+export default class EventForm extends Component {
+    handleAddPress = () => { }
+
+    render() {
+        return (
+            <View>
+                <TouchableHighlight onPress={this.handleAddPress}>
+                    <Text>Add</Text>
+                </TouchableHighlight>
+
+
+            </View>
+        )
+    }
+}
